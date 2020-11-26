2e28eda28fbc402fa682ddb8d3e8f5bd19036e82
claudiomansilla94 <75086218+claudiomansilla94@users.noreply.github.com>
2020-11-26 13:36:12 -0300

    Add files via upload

---
+++ data/CerrosdeAlhu_3.js
@@ -0,0 +1,8 @@
+var json_CerrosdeAlhu_3 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nombre": "Alto de Cantillana", "Altitud": "2281" }, "geometry": { "type": "Point", "coordinates": [ -70.966251499779517, -33.966796867896925 ] } },
+{ "type": "Feature", "properties": { "Nombre": "El Morro", "Altitud": "1975" }, "geometry": { "type": "Point", "coordinates": [ -70.989436421138535, -34.106765030678581 ] } }
+]
+}
